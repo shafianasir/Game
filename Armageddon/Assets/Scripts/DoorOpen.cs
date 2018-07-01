527d7aacc38a496ef9bd03f57ece8ab2eecc558f
ally0106 <38324061+ally0106@users.noreply.github.com>
2018-07-01 11:45:21 -0700

    b
    n

---
+++ Armageddon/Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour {
+
+	private Animator animation;
+
+	// Use this for initialization
+	void Start () {
+		animation = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter(Collider collide){
+		if (collide.tag == "Player") {
+			animation.SetBool ("Open", true);
+		}
+	}
+}
